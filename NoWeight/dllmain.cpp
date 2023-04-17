c91648a9e21d0047c9d527806aba2cccad7ecdf9
ImAxel0 <124681710+ImAxel0@users.noreply.github.com>
2023-04-17 16:13:17 +0200

    Updated

--- NoWeight/dllmain.cpp
+++ NoWeight/dllmain.cpp
@@ -1,31 +1,27 @@
-#include "pch.h"
-#include <Windows.h>
-
+#include "global.h"
 #include "ModUtils.h"
+#include "Detours/include/detours.h"
 
 using namespace ModUtils;
+
+LPVOID noWeightBack = nullptr;
 
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
 	Log("Activating NoWeight...");
-	std::vector<uint16_t> pattern = { 0x74, MASKED, 0xf3, 0x0f, 0x58, 0x70, MASKED, 0xff, 0xc7, 0x83, 0xff, MASKED, 0x7c, MASKED, 0x41, 0x8b, 0xfe };
-	std::vector<uint16_t> originalBytes = { 0x74, 0x05 };
-	std::vector<uint8_t> newBytes = { 0xeb, 0x05 };
+	std::vector<uint16_t> noWeight = { 0xff,0xc3,0x83,0xfb,MASKED,0x7c,MASKED,0x4c,0x8d,0x5c,0x24 };
+	uintptr_t noWeightAddr = SigScan(noWeight);
 
-	uintptr_t patchAddress = SigScan(pattern);
-	if (patchAddress != 0)
+	if (noWeightAddr != 0)
 	{
-		Replace(patchAddress, originalBytes, newBytes);
-	}
-	
-	std::vector<uint16_t> pattern1 = { 0x74, MASKED, 0xf3, 0x0f, 0x58, 0x70, MASKED, 0xff, 0xc7, 0x83, 0xff, MASKED, 0x7c, MASKED, 0x41, 0x8b, 0xde };
-	std::vector<uint16_t> originalBytes1 = { 0x74, 0x05 };
-	std::vector<uint8_t> newBytes1 = { 0xeb, 0x05 };
+		PDETOUR_TRAMPOLINE lpTrampolineData = {};
 
-	uintptr_t patchAddress1 = SigScan(pattern1);
-	if (patchAddress1 != 0)
-	{
-		Replace(patchAddress1, originalBytes1, newBytes1);
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		DetourAttachEx((PVOID*)&noWeightAddr, (PVOID)&noWeightAsm_func, &lpTrampolineData, nullptr, nullptr);
+		DetourTransactionCommit();
+		const auto lpDetourInfo = (DETOUR_INFO*)lpTrampolineData;
+		noWeightBack = lpDetourInfo->pbRemain;
 	}
 	CloseLog();
 	return 0;
